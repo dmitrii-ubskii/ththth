c99cafa577adfd3c1eeee0193e425d6925e3e59c
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-10-20 15:42:30 +0100

    multiplication & division

--- src/builtin.rs
+++ src/builtin.rs
@@ -8,11 +8,15 @@ use crate::{
 pub const DEFINE: GString = GString::from_bytes(b"define");
 const PLUS: GString = GString::from_bytes(b"+");
 const MINUS: GString = GString::from_bytes(b"-");
+const MULTIPLY: GString = GString::from_bytes(b"*");
+const DIVIDE: GString = GString::from_bytes(b"/");
 
 pub fn init_builtins() -> Env<'static> {
 	let mut env = Env::new();
 	env.insert(PLUS, Datum::Builtin(plus));
 	env.insert(MINUS, Datum::Builtin(minus));
+	env.insert(MULTIPLY, Datum::Builtin(multiply));
+	env.insert(DIVIDE, Datum::Builtin(divide));
 	env
 }
 
@@ -47,6 +51,44 @@ fn minus(args: &Expression, env: &Env<'_>) -> Result<Datum, EvaluationError> {
 					let right = plus(right, &env)?;
 					let Some(right) = right.as_number() else { unreachable!() };
 					Ok(Datum::Atom(Atom::Number(left - right)))
+				}
+				None => todo!(),
+			}
+		}
+	}
+}
+
+fn multiply(args: &Expression, env: &Env<'_>) -> Result<Datum, EvaluationError> {
+	let mut env = Env::local(env);
+	match args {
+		Expression::Atom(Atom::Nil) => Ok(Datum::Atom(Atom::Number(1.0))),
+		Expression::Atom(_) => todo!(),
+		Expression::Expression { left, right } => {
+			let left = eval(left, &mut env)?;
+			match left.as_number() {
+				Some(left) => {
+					let right = multiply(right, &env)?;
+					let Some(right) = right.as_number() else { unreachable!() };
+					Ok(Datum::Atom(Atom::Number(left * right)))
+				}
+				None => todo!(),
+			}
+		}
+	}
+}
+
+fn divide(args: &Expression, env: &Env<'_>) -> Result<Datum, EvaluationError> {
+	let mut env = Env::local(env);
+	match args {
+		Expression::Atom(Atom::Nil) => Ok(Datum::Atom(Atom::Number(1.0))),
+		Expression::Atom(_) => todo!(),
+		Expression::Expression { left, right } => {
+			let left = eval(left, &mut env)?;
+			match left.as_number() {
+				Some(left) => {
+					let right = multiply(right, &env)?;
+					let Some(right) = right.as_number() else { unreachable!() };
+					Ok(Datum::Atom(Atom::Number(left / right)))
 				}
 				None => todo!(),
 			}
